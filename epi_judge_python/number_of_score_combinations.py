9598634550a00648f56faa01f8fd74c16debd9a5
Noel Lin <nolin9889@gmail.com>
2021-01-15 16:32:22 -0800

    First problem, 16.1 Count the number of score combinations solved

--- epi_judge_python/number_of_score_combinations.py
+++ epi_judge_python/number_of_score_combinations.py
@@ -5,8 +5,22 @@ from test_framework import generic_test
 
 def num_combinations_for_final_score(final_score: int,
                                      individual_play_scores: List[int]) -> int:
-    # TODO - you fill in here.
-    return 0
+    if final_score == 0:
+        return 1
+
+    rows = len(individual_play_scores)
+    dp = [[0 for c in range(final_score + 1)] for r in range(rows + 1)]
+
+    for r in range(1, rows + 1):
+        for c in range(1, final_score + 1):
+            if c - individual_play_scores[r - 1] < 0:
+                dp[r][c] = dp[r - 1][c] + 0
+            elif c - individual_play_scores[r - 1] == 0:
+                dp[r][c] = dp[r - 1][c] + 1
+            else:
+                dp[r][c] = dp[r - 1][c] + dp[r][c - individual_play_scores[r - 1]]
+
+    return dp[rows][final_score]
 
 
 if __name__ == '__main__':
