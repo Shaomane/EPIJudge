dc81a1acbc821ab608bdb9761eb811861a7a032c
Noel Lin <nolin9889@gmail.com>
2021-01-19 08:19:57 -0800

    16.1 and 16.3 solutions

--- epi_judge_python/number_of_score_combinations.py
+++ epi_judge_python/number_of_score_combinations.py
@@ -5,6 +5,7 @@ from test_framework import generic_test
 
 def num_combinations_for_final_score(final_score: int,
                                      individual_play_scores: List[int]) -> int:
+    # TODO - you fill in here.
     if final_score == 0:
         return 1
 
@@ -12,14 +13,14 @@ def num_combinations_for_final_score(final_score: int,
     dp = [[0 for c in range(final_score + 1)] for r in range(rows + 1)]
 
     for r in range(1, rows + 1):
-        for c in range(1, final_score + 1):
+        for c in range(1, final_score + 1):d
             if c - individual_play_scores[r - 1] < 0:
                 dp[r][c] = dp[r - 1][c] + 0
             elif c - individual_play_scores[r - 1] == 0:
                 dp[r][c] = dp[r - 1][c] + 1
             else:
                 dp[r][c] = dp[r - 1][c] + dp[r][c - individual_play_scores[r - 1]]
-
+    
     return dp[rows][final_score]
 
 
--- epi_judge_python/number_of_traversals_matrix.py
+++ epi_judge_python/number_of_traversals_matrix.py
@@ -2,9 +2,17 @@ from test_framework import generic_test
 
 
 def number_of_ways(n: int, m: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    if n == 1 or m == 1:
+        return 1
 
+    # we really only want the first row and column set to 1,
+    # but it doesn't matter if we set the rest as well since they get overwritten.
+    dp = [[1] * (m)] * (n)
+    for r in range(1, n):
+        for c in range(1, m):
+            dp[r][c] = dp[r - 1][c] + dp[r][c - 1]
+
+    return dp[n - 1][m - 1]
 
 if __name__ == '__main__':
     exit(
