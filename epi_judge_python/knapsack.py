7fe5eea4a92188b577c4a94ed1062ae404ea9c28
Noel Lin <nolin9889@gmail.com>
2021-02-01 21:14:18 -0800

    Transferring to Lenovo ubuntu

--- epi_judge_python/knapsack.py
+++ epi_judge_python/knapsack.py
@@ -10,7 +10,17 @@ Item = collections.namedtuple('Item', ('weight', 'value'))
 
 def optimum_subject_to_capacity(items: List[Item], capacity: int) -> int:
     # TODO - you fill in here.
-    return 0
+    dp = [[0 for _ in range(capacity + 1)] for _ in range(len(items) + 1)]
+    
+    for r in range(1, len(items) + 1):
+        for c in range(1, capacity + 1):
+            currentAdd = 0
+            if c - items[r - 1].weight >= 0:
+                currentAdd = dp[r][c - items[r - 1].weight] + items[r - 1].value
+
+            dp[r][c] = max(dp[r - 1][c], dp[r][c - 1], currentAdd)
+
+    return dp[-1][-1]
 
 
 @enable_executor_hook
